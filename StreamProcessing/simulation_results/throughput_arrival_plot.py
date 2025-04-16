4c6bdfcba78987eb0bb83e1372f1214eda21e71f
NZAE000 <eliezer.zuniga@alumnos.uv.cl>
2025-04-16 03:04:37 -0400

    throughput script file modified: arrival rate metric added.

---
+++ StreamProcessing/simulation_results/throughput_arrival_plot.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+
+# === Functions ===
+
+def read_logs_throughput_arrival_rate(path):
+    records = []
+    with open(path, "r") as file:
+        for line in file:
+            if line.strip():
+                parts = line.strip().split()
+                rates = parts[0]
+                reqs = int(parts[1])
+                finish_time = float(parts[2])
+                throughput = float(parts[3])
+                time_last_arrival = float(parts[4])
+                arrival_rate = float(parts[5])
+
+                records.append((rates, reqs, finish_time, throughput, time_last_arrival, arrival_rate))
+    return records
+
+def show_table(records):
+    print("\nThroughput - Time Arrival:")
+    print(f"{'Arrival rates':<40} {'Requirements':<18} {'Finish time (s)':<20} {'Throughput (req/s)':<23} {'Time last arival (s)':<25} {'Arrival rate (req/s)':<20}")
+    print("-" * 152)
+    for rates, reqs, finish_time, throughput, time_last_arrival, arrival_rate in records:
+        print(f"{rates:50} {reqs:<15} {finish_time:<24.6f} {throughput:<24.2f} {time_last_arrival:<25.6f} {arrival_rate:<20.2f}") 
+
+def plot_throughput(records):
+    #x_labels = [f"{time:.4f}s\n[{rates}]" for rates, time, _, _ in records]
+    #y_values = [tp for _, _, _, tp in records]
+#
+    #plt.figure(figsize=(10, 6))
+    #plt.plot(x_labels, y_values, marker="o")
+    #plt.xlabel("Total time and Arrival rates")
+    #plt.ylabel("Throughput (req/s)")
+    #plt.title("Throughput by simulation configuration")
+    #plt.grid(True)
+    #plt.tight_layout()
+    #plt.xticks(rotation=45)
+    #plt.show()
+
+    x_labels = []
+    throughputs = []
+    tasas_llegada_real = []
+
+    prev_rates = None
+    for rates, _, finish_time, throughput, _, tasa_llegada_real in records:
+        if rates != prev_rates:
+            label = f"[{rates}]"
+            x_labels.append(label)
+            throughputs.append(throughput)
+            tasas_llegada_real.append(tasa_llegada_real)
+            prev_rates = rates
+
+    x = range(len(x_labels))
+
+    plt.figure(figsize=(10, 6))
+    #plt.style.use('dark_background')
+    plt.plot(x, throughputs, marker='o', label='Throughput (req/s)', color='blue')
+    plt.plot(x, tasas_llegada_real, marker='s', linestyle='--', label='Arrival Rate (req/s)', color='orange')
+
+    plt.xlabel("Rate")
+    plt.ylabel("(req/s)")
+    plt.title("Throughput and Arrival Rate")
+    plt.xticks(ticks=x, labels=x_labels, rotation=45)
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+# === File path ===
+throughput_path = "throughput.txt"
+
+# === Main execution ===
+
+records = read_logs_throughput_arrival_rate(throughput_path)
+show_table(records)
+
+plot_throughput(records)
+
--- StreamProcessing/simulation_results/throughput_arrival_record.py
+++ StreamProcessing/simulation_results/throughput_arrival_record.py
@@ -21,12 +21,12 @@ def extract_requirements_completed(state_file):
         lines = file.readlines()
 
     block = []
-    time_to_finish = None
-    productor_state = None
+    final_time = None
+    req_completed = None
 
     for line in reversed(lines): # Get from last line.
         if re.match(r"\d{2}:\d{2}:\d{2}:\d{3}:\d{3}:\d{3}:\d{3}:\d{3}", line.strip()):
-            time_to_finish = line.strip()
+            final_time = line.strip()
             block.append(line.strip())
             break
         else:
@@ -36,21 +36,21 @@ def extract_requirements_completed(state_file):
 
     for linea in block: # Detect productor state.
         if "State for model productor is" in linea:
-            productor_state = int(linea.split()[-1])
+            req_completed = int(linea.split()[-1])
             break
 
     # === Find the first time that state appears ===
     time_last_arrival = None
     for i in range(len(lines)):
-        if f"State for model productor is {productor_state}" in lines[i]:
+        if f"State for model productor is {req_completed}" in lines[i]:
             time_last_arrival = lines[i-1]
             break
     
-    #print("fini: ", time_to_finish, "last: ", time_last_arrival)
-    time_finish = parse_time_to_sec(time_to_finish) if time_to_finish else None
+    #print("fini: ", final_time, "last: ", time_last_arrival)
+    finish_time = parse_time_to_sec(final_time) if final_time else None
     time_last_arr = parse_time_to_sec(time_last_arrival) if time_last_arrival else None
 
-    return productor_state, time_finish, time_last_arr
+    return req_completed, finish_time, time_last_arr
 
 def extract_rates(path):
     rates = []
@@ -62,10 +62,11 @@ def extract_rates(path):
                     rates.append(rate)
     return rates
 
-def record_throughput(rates, reqs, time_finish, output_path):
+def record_throughput(rates, reqs, finish_time, time_last_arrival, output_path):
     rates_str = ";".join(rates)
-    throughput = reqs / time_finish if time_finish > 0 else 0
-    line = f"{rates_str} {reqs} {time_finish:.6f} {throughput:.5f}\n"
+    throughput = reqs / finish_time if finish_time > 0 else 0
+    arrival_rate = reqs / time_last_arrival if time_last_arrival > 0 else 0
+    line = f"{rates_str} {reqs} {finish_time:.6f} {throughput:.5f} {time_last_arrival:.6f} {arrival_rate:.5f} \n" 
     with open(output_path, "a") as file:
         file.write(line)
     return line.strip()
@@ -78,11 +79,10 @@ states_by_time_path = "simulation_results/system/system_test_output_state.txt"
 throughput_path = "throughput.txt"
 
 # === Main execution ===
-
-requirements_completed, time_finish, time_last_arr = extract_requirements_completed(states_by_time_path)
+requirements_completed, finish_time, time_last_arr = extract_requirements_completed(states_by_time_path)
 used_rates = extract_rates(arrival_periods_path)
-line_record = record_throughput(used_rates, requirements_completed, time_finish, throughput_path)
+line_record = record_throughput(used_rates, requirements_completed, finish_time, time_last_arr, throughput_path)
 
-print("\nNew record added to", throughput_path, ":")
+print("New record added to", throughput_path, ":")
 print(line_record)
 
